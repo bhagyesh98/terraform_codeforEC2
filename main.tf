8d62d179d14b16ac9997057bf05a57547d8d257e
Bhagyesh Patil <118748705+bhagyesh98@users.noreply.github.com>
2024-11-12 11:21:02 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-03753afda9b8ba740"
+instance_type = "t2.medium"
+key_name = "mumbaikey"
+vpc_security_group_ids = ["sg-0c8ec77dcaa64b1c5"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
+}
